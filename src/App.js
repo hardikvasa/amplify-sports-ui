25b11e9d5e96c0fe5787fbc7c5f1d054698ecaf7
Vasa <harniva@f8ffc20c25ae.ant.amazon.com>
2022-02-02 19:57:26 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -12,41 +12,6 @@ import awsConfig from './aws-exports'
 Amplify.configure(awsConfig)
 
 const App = () => {
-  const [name, setName] = useState('')
-  const [file, setFile] = useState('')
-  const [response, setResponse] = useState('')
-
-  const onChange = (e) => {
-    e.preventDefault()
-    if (e.target.files[0] !== null) {
-      setFile(e.target.files[0])
-      setName(e.target.files[0].name)
-    }
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault()
-    if (file) {
-      Storage.put(name, file, {
-        /* level: 'protected', */
-        contentType: file.type,
-      })
-        .then((result) => {
-          console.log(result)
-          setResponse(`Success uploading file: ${name}!`)
-        })
-        .then(() => {
-          document.getElementById('file-input').value = null
-          setFile(null)
-        })
-        .catch((err) => {
-          console.log(err)
-          setResponse(`Can't upload file: ${err}`)
-        })
-    } else {
-      setResponse(`Files needed!`)
-    }
-  }
 
   return (
     <AmplifyAuthenticator>
@@ -58,36 +23,8 @@ const App = () => {
         headerText='AnyCompany video team, Sign-Up with Your E-Mail Address'
         slot='sign-up'
       />
-      <div className='header'>
-        <h2>
-          <a href='/'>AnyCompany video admin panel</a>
-        </h2>
-      </div>
-      <div className='video-uploader'>
-        <form onSubmit={(e) => onSubmit(e)}>
-          <p>
-            <label className='select-label'>Select video: </label>
-          </p>
-          <p>
-            <input
-              className='video-input'
-              type='file'
-              id='file-input'
-              accept='video/*'
-              onChange={(e) => onChange(e)}
-            />
-          </p>
-          <button type='submit' className='btn'>
-            Submit <MdSend className='btn-icon' />
-          </button>
-        </form>
-      </div>
+      <h2>Hello</h2>
 
-      {response && (
-        <div id='upload-status' className='upload-status'>
-          {response}
-        </div>
-      )}
 
       <div className='sign-out'>
         <AmplifySignOut />
