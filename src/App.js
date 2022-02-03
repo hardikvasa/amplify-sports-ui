d483d1bd1c8d5fa84ef4936d8293a95f88e891c7
Vasa <harniva@f8ffc20c25ae.ant.amazon.com>
2022-02-02 20:15:20 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import './App.css'
 import Amplify, { Storage } from 'aws-amplify'
+import awsvideoconfig from './aws-video-exports'
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
 import {
   AmplifyAuthenticator,
   AmplifySignOut,
@@ -11,10 +14,22 @@ import { MdSend /* MdList */ } from 'react-icons/md'
 import awsConfig from './aws-exports'
 Amplify.configure(awsConfig)
 
-const App = () => {
 
-  return (
-    <AmplifyAuthenticator>
+class VideoPlayer extends React.Component {
+  componentDidMount() {
+    this.player = videojs(this.videoNode, this.props)
+  }
+
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  render() {
+    return (
+
+      <AmplifyAuthenticator>
       <AmplifySignIn
         headerText='AnyCompany video team, Sign-In with Your E-Mail Address'
         slot='sign-in'
@@ -23,14 +38,44 @@ const App = () => {
         headerText='AnyCompany video team, Sign-Up with Your E-Mail Address'
         slot='sign-up'
       />
-      <h2>Hello</h2>
-
+      <div data-vjs-player style={{
+          width: 960, height: 540
+        }}>
+        <video  ref={(node) => { this.videoNode = node; }} className="video-js" />
+      </div>
 
       <div className='sign-out'>
         <AmplifySignOut />
       </div>
     </AmplifyAuthenticator>
-  )
+
+    );
+  }
 }
+
+const videoJsOptions = {
+  autoplay: true,
+  controls: true,
+  sources: [{
+    src: awsvideoconfig.awsOutputLiveLL,
+  }]
+}
+
+function App() {
+  return (
+    <div>
+      <nav style={nav}>
+        <p style={navHeading}>Live Streaming with React & AWS</p>
+      </nav>
+      <div style={container}>
+        <VideoPlayer { ...videoJsOptions } />
+      </div>
+    </div>
+  );
+}
+
+const nav = { padding: '0px 40px', height: 60, borderBottom: '1px solid #ddd', display: 'flex', alignItems: 'center' }
+const container = { paddingTop: 40, width: 960, margin: '0 auto' }
+const navHeading = { margin: 0, fontSize: 18 }
 
 export default App
